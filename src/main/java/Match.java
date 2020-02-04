8ac385aa21a490c3cbd9fa9736be65b72f679e96
Cata16 <60660179+Cata16@users.noreply.github.com>
2020-02-04 18:20:51 +0200

    Add files via upload

---
+++ src/main/java/Match.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Catalin
+ */
+public class Match {
+    private int offset;
+    private int length;
+    private char ch;
+
+    public Match(int startIndex, int length, char ch) {
+        this.offset = startIndex;
+        this.length = length;
+        this.ch = ch;
+    }
+    
+
+    public char getCh() {
+        return ch;
+    }
+
+    public void setCh(char ch) {
+        this.ch = ch;
+    }
+   
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+    
+}
