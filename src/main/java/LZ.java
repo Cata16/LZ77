4b3207bdf611af31772028a4a340030803842419
Catalin <Catalin@DESKTOP-JF5S485>
2020-02-08 21:10:10 +0200

    Impementare cu lungime minima

--- src/main/java/LZ.java
+++ src/main/java/LZ.java
@@ -14,16 +14,17 @@ import java.util.logging.Logger;
 
 
 public class LZ {
- 
+
     static ArrayList<Byte> lookAheadBuffer = new ArrayList();
     static ArrayList<Byte> window = new ArrayList();
+    static ArrayList<Character> decodeWindow = new ArrayList();
     static FileReader reader = null;
     static BufferedWriter writer = null;
-    static int windowSize = 2048;
-    static int lookAheadSize = 2048;
- 
+    static int windowSize = 16000;
+    static int lookAheadSize = 16000;
+    static int minimumLeght=7;
+
     /**
-     *
      * @param args
      * @throws java.io.IOException
      */
@@ -33,67 +34,87 @@ public class LZ {
         File outputFileDecoded = new File("C:\\Users\\Catalin\\Desktop\\outStream1.txt");
         encode(inputFile, outputFileEncoded);
         decode(outputFileEncoded, outputFileDecoded);
- 
+
     }
- 
-    public static void decode(File inputFile, File outputFile) throws FileNotFoundException, IOException {
-        int offset = 0;
-        int length = 0;
-        int digitCount = 1;
-        ArrayList<Character> decodeWindow = new ArrayList();
+
+    public static void decode(File inputFile, File outputFile) throws IOException {
+
         reader = new FileReader(inputFile);
         writer = new BufferedWriter(new FileWriter(outputFile));
- 
+        Match match;
+        int counter=0;
         fillLookAheadBuffer(reader);
         int i;
         while (!lookAheadBuffer.isEmpty()) {
-            i = 0;
-            if (((char) (lookAheadBuffer.get(i) & 0xFF)) == '(') {
-                i++;
-                while (('0' <= lookAheadBuffer.get(i) && (lookAheadBuffer.get(i) <= '9'))) {
-                    offset = offset * digitCount + ((char) (lookAheadBuffer.get(i) & 0xFF) - '0');
-                    digitCount = 10;
- 
-                    i++;
-                }
-                digitCount = 1;
-                i++;
-                while (('0' <= lookAheadBuffer.get(i) && (lookAheadBuffer.get(i) <= '9'))) {
-                    length = length * digitCount + ((char) (lookAheadBuffer.get(i) & 0xFF) - '0');
-                    digitCount = 10;
-                    i++;
-                }
-                if (offset != 0) {
- 
-                    offset = decodeWindow.size() - offset;
-                    for (int j = offset; j < offset + length; j++) {
-                        writer.write(decodeWindow.get(j));
-                        decodeWindow.add(decodeWindow.size(), decodeWindow.get(j));
- 
-                    }
- 
-                }
-                i++;
-                writer.write((char) ((lookAheadBuffer.get(i) & 0xFF)));
-                decodeWindow.add(decodeWindow.size(), (char) ((lookAheadBuffer.get(i) & 0xFF)));
-                i += 2;
- 
-                digitCount = 1;
-                offset = 0;
-                length = 0;
-            } else {
-                writer.write((char) ((lookAheadBuffer.get(i) & 0xFF)));
-                decodeWindow.add(decodeWindow.size(), (char) ((lookAheadBuffer.get(i) & 0xFF)));
-                moveWindow(decodeWindow);
- 
-            }
+           i=0;
+                  if( ((char) (lookAheadBuffer.get(i) & 0xFF) )=='('  ) {
+                           match= searchForMatch();
+                   if(match==null){
+                       writer.write('(');
+                       decodeWindow.add(decodeWindow.size(),'(');
+                       i=1;
+                   }else{
+                       outputDecodedChars(match,decodeWindow);
+                       i=getNumberOfPositionsToMove(match);
+
+
+                       }
+                       }
+               else{
+               writer.write(lookAheadBuffer.get(i));
+               decodeWindow.add(decodeWindow.size(), ((char) (lookAheadBuffer.get(i) & 0xFF) ));
+               i=1;
+           }
+
             moveLookAheadBuffer(i);
+            moveWindow(decodeWindow);
         }
         writer.close();
         reader.close();
- 
+
     }
- 
+
+    private static int getNumberOfPositionsToMove(Match match) {
+        int counter=0;
+        if(match.getCh()==')'){
+            for(int j=0;j<lookAheadBuffer.size();j++){
+                if(lookAheadBuffer.get(j)==')'){
+                    counter++;
+                }
+                if(counter==2){
+                    return j+1;
+                }
+            }
+        }else{
+            return lookAheadBuffer.indexOf((byte)')')+1;
+        }
+        return -1;
+    }
+
+    private static Match searchForMatch() throws IOException {
+        int position=1;
+        String startIndex="";
+        String length="";
+        Match match=null;
+        while( (0<=((char) (lookAheadBuffer.get(position) & 0xFF) - '0'))&&(((char) (lookAheadBuffer.get(position) & 0xFF) - '0')<=9)){
+            startIndex += ((char) (lookAheadBuffer.get(position) & 0xFF) - '0');
+            position++;
+        }
+        if(((char) (lookAheadBuffer.get(position) & 0xFF) )!=','){
+            return match;
+        }
+        position++;
+        while( (0<=((char) (lookAheadBuffer.get(position) & 0xFF) - '0'))&&(((char) (lookAheadBuffer.get(position) & 0xFF) - '0')<=9)){
+            length += ((char) (lookAheadBuffer.get(position) & 0xFF) - '0');
+            position++;
+        }
+        if( (((char) (lookAheadBuffer.get(position) & 0xFF) )!=',')|| (((char) (lookAheadBuffer.get(position+2) & 0xFF) )!=')')){
+           return match;
+        }
+        match=new Match(Integer.parseInt(startIndex),Integer.parseInt(length),((char) (lookAheadBuffer.get(position+1) & 0xFF) ));
+        return match;
+        }
+
     public static void encode(File inputFile, File outputFile) throws IOException {
         try {
             reader = new FileReader(inputFile);
@@ -101,18 +122,22 @@ public class LZ {
             fillLookAheadBuffer(reader);
             while (!lookAheadBuffer.isEmpty()) {
                 Match match = findMatch(0);
+                outputEncodedChars(match);
                 moveLookAheadBuffer(match.getLength() + 1);
-                writer.write("(" + match.getOffset() + "," + match.getLength() + "," + match.getCh() + ")");
+                moveWindow(window);
             }
+
             writer.close();
             reader.close();
             displaySpaceSaved(inputFile, outputFile);
         } catch (FileNotFoundException ex) {
             Logger.getLogger(LZ.class.getName()).log(Level.SEVERE, null, ex);
         }
- 
+
+
     }
- 
+
+
     public static Match findMatch(int chPosition) {
         int offset = 0;
         int length = 0;
@@ -120,10 +145,10 @@ public class LZ {
         int lookAheadPosition;
         byte chToMatch = lookAheadBuffer.get(chPosition);
         Match match = new Match(offset, length, (char) (chToMatch & 0xFF));
- 
+
         if (!window.isEmpty()) {
             for (int i = window.size() - 1; i >= 0; i--) {
- 
+
                 if (window.get(i) == chToMatch) {
                     lookAheadPosition = chPosition;
                     offset = window.size() - i;
@@ -132,18 +157,18 @@ public class LZ {
                         length++;
                         lookAheadPosition++;
                         windowPosition++;
- 
+
                         if (lookAheadPosition >= lookAheadBuffer.size() || windowPosition >= window.size()) {
                             break;
                         }
- 
+
                     } while (Objects.equals(lookAheadBuffer.get(lookAheadPosition), window.get(windowPosition)));
- 
+
                     if ((length > match.getLength())) {
                         match.setLength(length);
                         match.setOffset(offset);
                         if (lookAheadPosition < lookAheadBuffer.size()) {
- 
+
                             match.setCh((char) (lookAheadBuffer.get(lookAheadPosition) & 0xFF));
                         } else {
                             match.setLength(length - 1);
@@ -154,49 +179,74 @@ public class LZ {
                 }
             }
         } else {
-            insertCharsInWindow(match, chToMatch);
+            insertCharsInWindow(match);
             return match;
         }
-        insertCharsInWindow(match, chToMatch);
-        moveWindow(window);
- 
+        insertCharsInWindow(match);
+
+
         return match;
     }
- 
-    public static void insertCharsInWindow(Match match, byte ch) {
-        // Insert new chars in window
+
+    private static void outputEncodedChars(Match match) throws IOException {
+        if (match.getLength() > minimumLeght) {
+            writer.write("(" + match.getOffset() + "," + match.getLength() + "," + match.getCh() + ")");
+        } else {
+
+            for (int i = (window.size() - match.getOffset() - match.getLength() - 1); i < (window.size() - match.getOffset() - 1); i++) {
+                writer.write((char) ((window.get(i) & 0xFF)));
+            }
+            writer.write(match.getCh());
+        }
+
+
+    }
+    private static void outputDecodedChars(Match match,ArrayList<Character> array) throws IOException {
+        int start=array.size() - match.getOffset() ;
+        int stop=start+ match.getLength() ;
+        for (int i = start; i < stop; i++) {
+            writer.write(array.get(i) );
+            array.add(array.size(),array.get(i) );
+
+        }
+        writer.write(match.getCh());
+        array.add(array.size(),match.getCh());
+
+    }
+
+    public static void insertCharsInWindow(Match match) {
         int start = window.size() - match.getOffset();
         int stop = window.size() - match.getOffset() + match.getLength();
         for (int i = start; i < stop; i++) {
             window.add(window.size(), window.get(i));
         }
         window.add(window.size(), (byte) match.getCh());
- 
+
     }
- 
+
     public static void fillLookAheadBuffer(FileReader inputReader) {
- 
+
         try {
             byte charValue;
             while (lookAheadBuffer.size() < lookAheadSize) {
                 if ((charValue = (byte) inputReader.read()) != (-1)) {
- 
+
                     lookAheadBuffer.add(charValue);
                 } else {
                     break;
                 }
- 
+
             }
         } catch (IOException ex) {
             Logger.getLogger(LZ.class.getName()).log(Level.SEVERE, null, ex);
         }
- 
+
     }
- 
+
     public static void moveWindow(ArrayList array) {
         if (array.size() > windowSize) {
             int numberOfPositions = array.size() - windowSize;
- 
+
             for (int i = 0; i < array.size() - numberOfPositions; i++) {
                 Collections.swap(array, i, i + numberOfPositions);
             }
@@ -205,26 +255,26 @@ public class LZ {
             }
         }
     }
- 
+
     public static void displaySpaceSaved(File inputFile, File outputFile) {
         long inputSize = inputFile.length();
         long outputSize = outputFile.length();
         long percentageDifference = ((100 * (inputSize - outputSize)) / inputSize);
-        System.out.print((inputSize - outputSize) + "  bytes Saved" + "  " + percentageDifference);
- 
+        System.out.print((inputSize - outputSize) + "  bytes Saved" + "  " + percentageDifference+"%");
+
     }
- 
+
     private static void moveLookAheadBuffer(int numberOfPositions) {
- 
+
         for (int i = 0; i < lookAheadBuffer.size() - numberOfPositions; i++) {
- 
+
             Collections.swap(lookAheadBuffer, i, i + numberOfPositions);
         }
         for (int i = 0; i < numberOfPositions; i++) {
             lookAheadBuffer.remove(lookAheadBuffer.size() - 1);
         }
         fillLookAheadBuffer(reader);
- 
+
     }
- 
+
 }
